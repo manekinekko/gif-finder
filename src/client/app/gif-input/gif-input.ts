17e36ff4d6ef474b841023df7fcabc25c9031e9a
Wassim Chegham <github@wassimchegham.com>
2016-04-21 21:36:06 +0200

    refacto: update component logic

--- src/client/app/gif-input/gif-input.ts
+++ src/client/app/gif-input/gif-input.ts
@@ -1,42 +1,36 @@
-import {Component} from 'angular2/core';
-
+import {Component, Output, EventEmitter} from 'angular2/core';
+import {SpeechToText} from '../speech-to-text/speech-to-text';
+import {IS_SPEECH_SUPPORTED} from '../config'
 
 @Component({
   selector: 'gif-input',
   templateUrl: 'app//gif-input/gif-input.html',
   styleUrls: ['app//gif-input/gif-input.css'],
   providers: [],
-  directives: [],
+  directives: [SpeechToText],
   pipes: []
 })
 export class GifInput {
 
-  @Output('onSearch') search: EventEmitter<string> = new EventEmitter<string>();
-  @Output('onError') error: EventEmitter<string> = new EventEmitter<string>();
+  @Output('onSearch') search$: EventEmitter<string> = new EventEmitter<string>();
+  @Output('onError') error$: EventEmitter<string> = new EventEmitter<string>();
+
+  private is_speeech_supported;
 
   constructor() {
     this.is_speeech_supported = IS_SPEECH_SUPPORTED;
   }
 
   onChanged(e) {
-    this.search.emit(e.target.value);
+    this.search$.emit(e.target.value);
   }
 
   onSpeechOK(tag) {
-    this.search.emit(tag);
+    this.search$.emit(tag);
   }
 
   onSpeechKO(hint) {
-    this.error.emit(hint);
+    this.error$.emit(hint);
   }
-
-  /*
-
-  this.results = someFormInput.valueChanges //observable of input changes
-    .debounceTime(150) //debounce for 150ms
-    .switchMap(query => http.get(`http://api.com?q=${query}`)) //make request
-    .map(res => res.json()); //map to json
-
-  */
 
 }
