ec65d16003c102f95c5a54e6d7fc4e035961e5df
Wassim Chegham <github@wassimchegham.com>
2016-04-24 19:58:23 +0200

    build: import angular2-speech-engine

--- angular-cli-build.js
+++ angular-cli-build.js
@@ -6,8 +6,14 @@ module.exports = function(defaults) {
   var app = new Angular2App(defaults, {
     vendorNpmFiles: [
       '@angular2-material/core/core.js',
-      '@angular2-material/toolbar/toolbar.js'
+      '@angular2-material/toolbar/toolbar.js',
+      '@angular2-material/card/card.js',
+      '@angular2-material/button/button.js',
+      'angular2-speech-engine/dist/angular2-speech-engine.js',
+      'angular2-speech-engine/dist/src/core.js',
+      'angular2-speech-engine/dist/src/engine/abstract-speech-engine.js',
+      'angular2-speech-engine/dist/src/engine/native-speech-engine.js'
     ]
   });
-  return app.toTree();
+  return app;
 };
