a5299363acddd7c321bfa2c4b601f42f1146c1ee
Wassim Chegham <github@wassimchegham.com>
2016-04-24 20:00:53 +0200

    chore: improve the gif-finder component

--- src/client/app/gif-finder.ts
+++ src/client/app/gif-finder.ts
@@ -1,21 +1,24 @@
 import {Component} from 'angular2/core';
 import {MdToolbar} from '@angular2-material/toolbar';
 import {GifImage} from './gif-image/gif-image';
-import {GifInput} from './gif-input/gif-input';
+import {SpeechToText} from './speech-to-text/speech-to-text';
 import {API} from './api/api';
+import {IS_SPEECH_SUPPORTED} from './config'
 
 @Component({
   selector: 'gif-finder-app',
   templateUrl: 'app/gif-finder.html',
   styleUrls: ['app/gif-finder.css'],
-  directives: [GifImage, GifInput, MdToolbar]
+  directives: [GifImage, SpeechToText, MdToolbar]
 })
 export class GifFinderApp {
 
   private tag: string;
   private hint: string;
+  private is_speeech_supported;
 
   contructor(){
+    this.is_speeech_supported = IS_SPEECH_SUPPORTED;
     this.tag = '';
     this.hint = '';
   }
